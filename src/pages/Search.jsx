a5f3d50efab276bedad25fe93cc73b40db99995a
Victor Matias <fvictor1705@gmail.com>
2022-08-01 21:14:46 -0300

    Resolve requisito 5.

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,53 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentSearch: '',
+      isButtonDisabled: true,
+    };
+  }
+
+  handleSubimit = (event) => {
+    event.preventDefault();
+  }
+
+  enableButton = () => {
+    this.setState((prevState) => {
+      const { currentSearch } = prevState;
+      const parameter = 2;
+      const isTrue = currentSearch.length < parameter;
+      return { isButtonDisabled: isTrue };
+    });
+  }
+
+  inputOnChange = ({ target }) => {
+    this.setState({ [target.name]: target.value }, this.enableButton);
+  }
+
   render() {
+    const { isButtonDisabled } = this.state;
+
     return (
       <div data-testid="page-search">
         <Header />
-        search
+        <form onSubmit={ this.handleSubimit } action="search-albums">
+          <input
+            type="text"
+            name="currentSearch"
+            placeholder="Procure por bandas ou artistas"
+            onChange={ this.inputOnChange }
+            data-testid="search-artist-input"
+          />
+          <button
+            type="submit"
+            disabled={ isButtonDisabled }
+            data-testid="search-artist-button"
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
